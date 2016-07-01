3149c2510d7fcd787f84b420cb8b0eec2b3123ec
Evgeniy Titov <falgore88@gmail.com>
2016-07-01 16:23:09 +0300

    new version 0.0.23

--- selectel_storage/base/__init__.py
+++ selectel_storage/base/__init__.py
@@ -64,10 +64,16 @@ class SelectelApi(object):
                 self.connection.authenticate()
             raise
 
+    @retry((ReadTimeout, HTTPError), delay=1.5)
     def info(self):
         url = "%s/%s" % (self.connection.auth.storage, self.container)
-        r = self.connection.session.head(url, verify=True)
-        r.raise_for_status()
+        try:
+            r = self.connection.session.head(url, verify=True)
+            r.raise_for_status()
+        except HTTPError as e:
+            if e.response.status_code == 401:
+                self.connection.authenticate()
+            raise
         result = {
             'used': int(r.headers['X-Container-Bytes-Used']),
             'quota': int(r.headers['X-Container-Meta-Quota-Bytes']),
